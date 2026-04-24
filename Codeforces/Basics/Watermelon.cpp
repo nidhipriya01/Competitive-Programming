c788b1317bdf5f1a36a71f120eef7e1b37d8760d
nidhipriya01 <nidhipriya2k6@gmail.com>
2026-04-25 00:33:09 +0530

    Create Watermelon.cpp

---
+++ Codeforces/Basics/Watermelon.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define endl '\n'
+#define mod 1e9+7
+
+void solve(){
+  int w;
+  cin >> w;
+
+  if (w%2==0 && w!=2) {       //w>2 
+    cout<<"YES"<<endl;
+  }
+  else{
+    cout<<"NO"<<endl;
+  }
+
+}
+
+signed main() {
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+    // int t;cin >> t;
+    // while (t--)
+       solve();
+
+
+    return 0;
+}

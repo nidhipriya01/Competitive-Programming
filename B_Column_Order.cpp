3811ecc41d14031c17fe04d8b2079bcbfd5a9850
nidhipriya01 <nidhipriya2k6@gmail.com>
2026-07-22 12:58:36 +0530

    Add files via upload

---
+++ B_Column_Order.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m ,i,j;
+    cin >> n >> m;
+
+    int arr[n][m];
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << arr[j][i] << " ";
+        }
+       
+    }
+
+    return 0;
+}

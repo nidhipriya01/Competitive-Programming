17a7cba7dc8049539915e86f3a4a66a70834debf
Nidhi Priya <nidhipriya2k6@gmail.com>
2026-07-31 13:38:50 +0530

    Add files via upload

---
+++ N_Max_and_Min_of_2_Numbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+int a;
+int b;
+cin >> a >> b ;
+if(a>b)
+{
+    cout <<"Min = " << b << endl;
+    cout <<"Max = " << a << endl;
+
+}
+if (a<b)
+{
+    cout << "Min = "<< a<< endl;
+    cout << "Max = "<< b << endl;
+}
+
+
+
+}

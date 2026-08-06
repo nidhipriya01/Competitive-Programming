7744f71fb8e9cc91a938c3f9021e724b4c864f52
Nidhi Priya <nidhipriya2k6@gmail.com>
2026-08-06 23:33:16 +0530

    Add files via upload

---
+++ K_Print_Number_in_Reverse.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+ long long int n;
+if (!(cin >> n)) return 0;
+if (n==0){
+    cout << 0;
+} else {
+while (n!=0){
+cout << n%10;
+n=n/10;
+
+ 
+ }
+    
+}
+
+}

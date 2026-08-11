795641b51dde5d5ace6536105ceff761cf3a3727
Nidhi Priya <nidhipriya2k6@gmail.com>
2026-08-11 23:12:35 +0530

    Add files via upload

---
+++ J_Multiples.cpp
@@ -0,0 +1,14 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+int n1,n2;
+cin >> n1 >> n2;
+if( n1 % n2 == 0 || n2 % n1 == 0){
+cout << "Multiples" << endl;
+} else {
+     cout << "No Multiples" << endl; 
+}
+
+}

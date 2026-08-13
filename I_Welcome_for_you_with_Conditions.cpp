7120a1c77fb769672b214644febb3be3cff73fc6
Nidhi Priya <nidhipriya2k6@gmail.com>
2026-08-13 23:43:30 +0530

    Add files via upload

---
+++ I_Welcome_for_you_with_Conditions.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+int n1,n2;
+cin >> n1 >> n2 ;
+ if( n1 >= n2) 
+ {
+cout << "Yes"<< endl;
+}else {
+ 
+cout << "No" << endl  ;
+}
+return 0;
+
+
+
+}

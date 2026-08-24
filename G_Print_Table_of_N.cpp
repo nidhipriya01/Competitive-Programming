922166c1853613a2cafed1732d0335e55af720b5
Nidhi Priya <nidhipriya2k6@gmail.com>
2026-08-24 19:36:28 +0530

    Add files via upload

---
+++ G_Print_Table_of_N.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+
+for (int i=1;i<=10;i++){
+    int result =n*i;
+    cout << n << " * " << i << " = " << result << endl;
+}
+
+
+
+
+
+}

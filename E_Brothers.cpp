9b3c4f5a3c3c8dfca1b9faa3bbd7c27508cfec62
Nidhi Priya <nidhipriya2k6@gmail.com>
2026-08-31 14:41:28 +0530

    Add files via upload

---
+++ E_Brothers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main (){
+string s1,t1,s2,t2;
+cin >> s1>>t1;
+cin>>s2>>t2;
+if (t1==t2)
+{
+    cout <<"YES"<< endl ;
+
+}
+else
+{
+    cout << "NO";
+}
+
+return 0;
+
+}
+

3fd325e33958b20f90d247c9e769a7837c591dcd
Nidhi Priya <nidhipriya2k6@gmail.com>
2026-09-07 18:38:09 +0530

    Add files via upload

---
+++ E_Area_of_a_Circle.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long double r,pi, area;
+    
+pi=3.141592653;
+cin >> r ;
+area= pi *r*r ;
+cout<< fixed<< setprecision(9); // 9 digit after decimal
+cout<< area << endl;
+
+return 0;
+}
